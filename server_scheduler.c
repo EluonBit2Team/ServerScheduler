cfebdc0b5de429789245b92025908a6d607f34e5
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2024-07-03 11:45:58 +0900

    gg

--- server_scheduler.c
+++ server_scheduler.c
@@ -249,7 +249,7 @@ void save_to_db(float max_cpu, float avg_cpu, float max_db, float avg_db, float 
     printf("mysql_real_connect end\n");
     char query[512];
     snprintf(query, sizeof(query),
-            "INSERT INTO statistic (tps_avg, tps_max, mem_avg, mem_max, login_user_cnt_avg, login_user_cnt_max) VALUES (%f, %f, %f, %f, %f, %f)"
+            "INSERT INTO statistic (tps_avg, tps_max, mem_avg, mem_max, login_user_cnt_avg, login_user_cnt_max, log_timestamp) VALUES (%f, %f, %f, %f, %f, %f, NOW())"
             ,random_float(), random_float(), max_mem, avg_mem, random_float(), random_float());
     printf("%s\n", query);
     if (mysql_query(conn, query)) {
